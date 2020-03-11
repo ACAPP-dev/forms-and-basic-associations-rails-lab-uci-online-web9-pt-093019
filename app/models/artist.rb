7290b1130f75bfb62d320ac3565cd5d900a03f59
ACAPP-dev <acapp909@gmail.com>
2020-03-11 16:51:55 -0700

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,12 @@
 class Artist < ActiveRecord::Base
   # add associations here
+  has_many :songs
+
+  def self.find_or_create_by(name)
+    if Artist.find_by(name)
+      artist = Artist.find_by(name)
+    else
+      artist = Artist.create(name)
+    end
+  end
 end
